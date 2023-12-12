2287ba0d6cc2674b2686fa6f38a3c1da450fdcb4
Leeladhar_290 <leeladharjagarapu@gmail.com>
2023-12-13 00:43:27 +0530

    Aspireo setup

---
+++ backend/index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const cors = require("cors");
+const { connection } = require("./connection");
+const { userRouter } = require("./Routes/user.routes");
+const { taskRouter } = require("./routes/task.routes");
+const { projectRouter } = require("./routes/project.routes");
+
+require("dotenv").config();
+
+const PORT = process.env.PORT || 7070;
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/users", userRouter);
+app.use("/tasks", taskRouter);
+app.use("/project", projectRouter);
+
+app.get("/", (req, res) => {
+  try {
+    res.status(200).send("Server is running");
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+app.listen(PORT, async () => {
+  try {
+    await connection;
+    console.log(`Server is connected to PORT: ${PORT}`);
+  } catch (error) {
+    console.log("Index Error", error.message);
+  }
+});
