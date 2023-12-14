f367c58d8b5ea4e49a8a26e8accefd0887bb56f4
hrutik2 <hrutik0729@gmail.com>
2023-12-14 11:47:21 +0530

    d

--- backend/model/blacklist.model.js
+++
@@ -1,9 +0,0 @@
-const mongoose=require("mongoose")
-
-const tokenSchema=mongoose.Schema({
-   token:String,
-   
-})
-const tokenModel=mongoose.connect("token",tokenSchema)
-
-module.exports={tokenModel}
