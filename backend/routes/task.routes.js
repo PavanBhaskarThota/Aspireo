355de8a84da9f96fd9f0954a7d7806a427ab376a
Leeladhar_290 <leeladharjagarapu@gmail.com>
2023-12-14 21:29:35 +0530

    task schema added

--- backend/routes/task.routes.js
+++ backend/routes/task.routes.js
@@ -1,5 +1,23 @@
 const express = require("express");
+const { TaskModel } = require("../model/task.model");
 
 const taskRouter = express.Router();
+
+
+taskRouter.post('/add', async(req,res)=>{
+    try {
+          const createdAt= new Date();
+          req.body.createdAt= createdAt;
+          console.log(createdAt)
+          const newPost= new TaskModel(req.body);
+          await newPost.save();
+
+          res.status(200).send(newPost);
+
+    }catch(error){
+        res.status(400).send(error.message);
+    }
+})
+
 
 module.exports = { taskRouter };
