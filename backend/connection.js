a08af09b0aa4933ec46c9132328419cf508da9bd
Rutik Jadhav <112790437+hrutik2@users.noreply.github.com>
2023-12-17 19:31:17 +0530

    Update connection.js

--- backend/connection.js
+++ backend/connection.js
@@ -2,6 +2,6 @@ const mongoose=require("mongoose")
 
 require("dotenv").config();
 
-//const connection = mongoose.connect(process.env.mongoURL);
-const connection = mongoose.connect("mongodb+srv://hrutik0729:hrutik@cluster0.9kopobq.mongodb.net/NEM_111(121)?retryWrites=true&w=majority");
+const connection = mongoose.connect(process.env.mongoURL);
+
 module.exports = { connection };
