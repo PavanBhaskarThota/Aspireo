9d2343fb9615c8b4ee63abcccf8e0c5212e6f4e4
PavanBhaskarThota <pavanbhaskarthota05@gmail.com>
2023-12-18 19:22:35 +0530

    projectsCss

--- frontend/src/components/ProjectCard.jsx
+++ frontend/src/components/ProjectCard.jsx
@@ -1,72 +1,123 @@
-
-
-import React, { useState } from 'react';
-import { Box, Button, Heading, Text, Input, Flex, useToast } from '@chakra-ui/react';
-import { useDispatch } from 'react-redux';
-import { updateProjectChatFun } from '../redux/projectReducer/action';
+import React, { useState } from "react";
+import {
+  Box,
+  Button,
+  Heading,
+  Text,
+  Input,
+  Flex,
+  useToast,
+} from "@chakra-ui/react";
+import { useDispatch } from "react-redux";
+import { updateProjectChatFun } from "../redux/projectReducer/action";
 
 const ProjectCard = ({ project, onAddCollaborator, onAddTask, users }) => {
-  const { name, description, colleborators,chats } = project;
+  const { name, description, colleborators, chats } = project;
 
   const dispatch = useDispatch();
-  const toast= useToast();
+  const toast = useToast();
 
-  const token = localStorage.getItem('token');
+  const token = localStorage.getItem("token");
   const userData = JSON.parse(localStorage.getItem("user"));
 
-   const collaboratorUsernames = colleborators.map((collaboratorId) => {
-      const user = users.find((user) => user._id === collaboratorId);
-      return user ? user.userName : collaboratorId;
-    });
-
+  const collaboratorUsernames = colleborators.map((collaboratorId) => {
+    const user = users.find((user) => user._id === collaboratorId);
+    return user ? user.userName : collaboratorId;
+  });
 
   const [chatMessages, setChatMessages] = useState(chats);
-  const [newMessage, setNewMessage] = useState('');
+  const [newMessage, setNewMessage] = useState("");
 
   const handleSendMessage = () => {
-    if (newMessage.trim() !== '') {
+    if (newMessage.trim() !== "") {
       setChatMessages((prevMessages) => {
-        const updatedMessages = [...prevMessages, `${userData.userName} : ${newMessage}`];
+        const updatedMessages = [
+          ...prevMessages,
+          `${userData.userName} : ${newMessage}`,
+        ];
         console.log(updatedMessages);
-        const newChat= {chats:updatedMessages}
-        dispatch(updateProjectChatFun(newChat,project._id,token,toast));
+        const newChat = { chats: updatedMessages };
+        dispatch(updateProjectChatFun(newChat, project._id, token, toast));
         return updatedMessages;
       });
-      setNewMessage('');
+      setNewMessage("");
     }
   };
 
   return (
-    <Box borderWidth="1px" borderRadius="lg" overflow="hidden" p={4} mb={4}>
-      <Heading size="md">{name}</Heading>
-      <Text mt={2} color="gray.600">
-        {description}
-      </Text>
+    <Box borderRadius="20px" overflow="hidden" p={5} mb={4} bg={'#FAF6F0'} w={'90%'} m={'auto'} mt={10} color={'white'}>
+      <Flex justifyContent={"space-between"} w={"100%"} m={"auto"} gap={5}>
+        <Flex w={"30%"} flexDirection={'column'} justifyContent={'space-between'}>
+          <Box>
+            <Heading size="lg">{name}</Heading>
+            <Text mt={2} color="gray.600">
+              {description}
+            </Text>
+          </Box>
+          <Box>
+            <Button
+              size="sm"
+              mt={4}
+              mb={5}
+              h={'40px'}
+              borderRadius={'25px'}
+              bg={'#0e2a61e6'}
+              colorScheme="blue"
+              onClick={() => onAddCollaborator(project)}
+            >
+              Add Collaborator
+            </Button>
+          </Box>
+        </Flex>
 
-      <Text>Collaborators:</Text>
-      {collaboratorUsernames.length > 0 &&collaboratorUsernames.map((collaborator, index) => (
-          <Text key={index}>{collaborator}</Text>
-        ))}
+        <Box w={"30%"}>
+          <Text fontSize={"xl"} fontWeight={"500"} color={'#2D4356'}>
+            Collaborators:
+          </Text>
 
-      <Button
-        size="sm"
-        mt={4}
-        colorScheme="blue"
-        onClick={() => onAddCollaborator(project)}
-      >
-        Add Collaborator
-      </Button>
+          {collaboratorUsernames.length > 0 &&
+            collaboratorUsernames.map((collaborator, index) => (
+              <Button display={"block"} mt={2} key={index} borderRadius={'20px'} color={'white'} bg={'#2D4356'}>
+                {" "}
+                {index + 1}. {collaborator}
+              </Button>
+            ))}
+        </Box>
 
-      <Box mt={4}>
+        <Box w={"40%"} bg={'#2D4356'} p={5} borderRadius={'20px'}>
+          <Text fontWeight="bold" mb={2}>
+            Chat:
+          </Text>
+          <Box  p={2} overflow={"scroll"} maxH={"250px"}>
+            {chatMessages.length > 0 &&
+              chatMessages.map((message, index) => (
+                <Text key={index} w={"80%"}>
+                  {message}
+                </Text>
+              ))}
+          </Box>
+          <Flex mt={2}>
+            <Input
+              value={newMessage}
+              size="md"
+              onChange={(e) => setNewMessage(e.target.value)}
+              placeholder="Type your message..."
+            />
+            <Button ml={2} onClick={handleSendMessage}>
+              Send
+            </Button>
+          </Flex>
+        </Box>
+      </Flex>
+
+      {/* <Box mt={4}>
         <Text fontWeight="bold" mb={2}>
           Chat:
         </Text>
-        <Box borderWidth="1px" p={2} overflow={'scroll'} maxH={'250px'}>
+        <Box borderWidth="1px" p={2} overflow={"scroll"} maxH={"250px"}>
           {chatMessages.length > 0 &&
             chatMessages.map((message, index) => (
-              <Text key={index}>
-                {message}
-              </Text>
+              <Text key={index} w={'20%'}>{message}</Text>
             ))}
         </Box>
         <Flex mt={2}>
@@ -80,7 +131,7 @@ const ProjectCard = ({ project, onAddCollaborator, onAddTask, users }) => {
             Send
           </Button>
         </Flex>
-      </Box>
+      </Box> */}
     </Box>
   );
 };
